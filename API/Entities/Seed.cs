5c2bbee7665463fe895ecb4231a51570de2724e8
JesusA-rc <jadrianc777@gmail.com>
2026-04-05 13:29:09 -0700

    End of section 8

---
+++ API/Entities/Seed.cs
@@ -0,0 +1,57 @@
+using System.Text;
+using System.Text.Json;
+using API.Data;
+using Microsoft.EntityFrameworkCore;
+
+namespace API.Entities;
+
+public class Seed
+{
+    public static async Task SeedUsers(AppDbContext context)
+    {
+        if (await context.Users.AnyAsync()) return;
+
+        var memberData = await File.ReadAllTextAsync("Data/userSeedData.json");
+        var members = JsonSerializer.Deserialize<List<SeedUserDto>>(memberData);
+
+        if(members == null) return;
+
+        using var hmac = new System.Security.Cryptography.HMACSHA512();
+        
+        foreach (var member in members)
+        {
+            var user = new AppUser
+            {
+                Id = member.Id,
+                Email = member.Email,
+                ImageUrl = member.ImageUrl,
+                DisplayName = member.DisplayName,
+                PasswordHash = hmac.ComputeHash(Encoding.UTF8.GetBytes("Pa$$w0rd")),
+                PasswordSalt = hmac.Key,
+                Member = new Member
+                {
+                    Id = member.Id,
+                    MyProperty = DateOnly.Parse(member.DateOfBirth),
+                    ImageUrl = member.ImageUrl,
+                    DisplayName = member.DisplayName,
+                    Created = member.Created,
+                    LastActive = member.LastActive,
+                    Gender = member.Gender,
+                    Description = member.Description,
+                    City = member.City,
+                    Country = member.Country
+                }
+            };
+
+            user.Member.Photos.Add(new Photo
+            {
+                Url = member.ImageUrl!,
+                MemberId = member.Id
+            });
+
+            context.Users.Add(user);
+        }
+
+        await context.SaveChangesAsync();
+    }
+}
--- API/Program.cs
+++ API/Program.cs
@@ -2,6 +2,7 @@ using API.Data;
 using API.Interfaces;
 using API.Middleware;
 using API.Services;
+using API.Entities;
 using Microsoft.AspNetCore.Authentication.JwtBearer;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.IdentityModel.Tokens;
@@ -19,6 +20,7 @@ builder.Services.AddDbContext<AppDbContext>(opt =>
 
 builder.Services.AddCors();
 builder.Services.AddScoped<ITokenService, TokenService>();
+builder.Services.AddScoped<IMemberRepository, MemberRepository>();
 builder.Services.AddAuthentication(JwtBearerDefaults.AuthenticationScheme)
     .AddJwtBearer(options =>
     {
@@ -48,5 +50,19 @@ app.UseAuthentication();
 app.UseAuthorization();
 
 app.MapControllers();
+
+using var scope = app.Services.CreateScope();
+var services = scope.ServiceProvider;
+try
+{
+    var context = services.GetRequiredService<AppDbContext>();
+    await context.Database.MigrateAsync();
+    await Seed.SeedUsers(context);
+}
+catch (Exception ex)
+{
+    var logger = services.GetRequiredService<ILogger<Program>>();
+    logger.LogError(ex, "An error occurred during migration");
+}
 
 app.Run();
